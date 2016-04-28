a11b66a585b25d256b0874a5428907472a099446
Espen Dalløkken <espen.dallokken@nrk.no>
2016-04-28 14:06:00 +0200

    adding challenge

--- bin/server.js
+++ bin/server.js
@@ -8,8 +8,8 @@ app.get('/', function(req, res){
   res.send('How may I be of service');
 });
 app.get('/fb', function(req, res){
-  console.log('FB Request', req);
-  res.status(200).send('1772547061');
+  console.log('FB Request', req.query['hub.challenge']);
+  res.status(200).send(req.query['hub.challenge']);
 });
 
 app.listen(port, function(){
