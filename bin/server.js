5e94c03db4085faa2a7f0bdd359a60a5f8991362
Espen Dalløkken <espen.dallokken@nrk.no>
2016-04-28 14:20:23 +0200

    spelling

--- bin/server.js
+++ bin/server.js
@@ -19,14 +19,14 @@ app.get('/fb', function(req, res){
 });
 
 var events, event;
-app.post('/fb', fuction(req, res){
+app.post('/fb', function(req, res){
   events = req.body.entry[0].messaging;
   for (var i=0; i < events.length; i++) {
     event = req.body.entry[0].messaging[i];
+    console.log('Event', event);
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-
       console.log('Message', text);
     }
   }
