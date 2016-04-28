2a8d59b81bc61a2989fa8734a26fc189e8f2aedc
Espen Dalløkken <espen.dallokken@nrk.no>
2016-04-28 14:23:47 +0200

    adding body parser

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var port = process.env.PORT || 9090;
 
