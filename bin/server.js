163482eaee2b7feb96b10d910abe0c0875a5b708
Espen Dalløkken <espen.dallokken@nrk.no>
2016-04-28 13:53:45 +0200

    using env port

--- bin/server.js
+++ bin/server.js
@@ -2,11 +2,13 @@ var express = require('express');
 
 var app = express();
 
+var port = process.env.PORT || 9090;
+
 app.get('/fb', function(req, res){
   console.log('FB Request', req);
   res.status(200).send('');
 });
 
-app.listen(3030, function(){
+app.listen(port, function(){
   console.log('I am listening....');
 });
