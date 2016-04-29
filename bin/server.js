d9b4311b38dede2692c9052daf9e2decddd96651
Espen Dalløkken <espen.dallokken@nrk.no>
2016-04-29 10:14:52 +0200

    .......

--- bin/server.js
+++ bin/server.js
@@ -84,46 +84,24 @@ function createButtonedReply(){
        "template_type":"generic",
        "elements":[
          {
-           "title":"Classic White T-Shirt",
+           "title":"Vad tycket du om våran svar?",
            "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
-           "subtitle":"Soft white cotton t-shirt is back in style",
+           "subtitle":"Vi vil gärna veta hur bra du tycket våran svar va.",
            "buttons":[
              {
                "type":"web_url",
                "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
-               "title":"View Item"
+               "title":"Jette bra!"
              },
              {
                "type":"web_url",
                "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
-               "title":"Buy Item"
+               "title":"Dårligt"
              },
              {
                "type":"postback",
-               "title":"Bookmark Item",
+               "title":"Inte relevant",
                "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM100"
-             }
-           ]
-         },
-         {
-           "title":"Classic Grey T-Shirt",
-           "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
-           "subtitle":"Soft gray cotton t-shirt is back in style",
-           "buttons":[
-             {
-               "type":"web_url",
-               "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
-               "title":"View Item"
-             },
-             {
-               "type":"web_url",
-               "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
-               "title":"Buy Item"
-             },
-             {
-               "type":"postback",
-               "title":"Bookmark Item",
-               "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM101"
              }
            ]
          }
